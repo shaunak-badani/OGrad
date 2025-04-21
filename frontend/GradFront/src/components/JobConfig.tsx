381b93cc8a109707e283172e1a97cf1edc87355e
Shaunak Badani <shaunak.badani@duke.edu>
2025-04-21 10:35:30 -0400

    Implemented UI

--- frontend/GradFront/src/components/JobConfig.tsx
+++ frontend/GradFront/src/components/JobConfig.tsx
@@ -1,17 +1,94 @@
+import backendClient from '@/utils/backendClient';
 import { Button } from './ui/button'
-import axios from 'axios';
+import { useState, useRef } from 'react';
+import Layers from './Layers';
+import AddLayer from './AddLayer';
+
+
 
 const JobConfig = (props) => {
 
-    
+    const [job, setJob] = useState({
+        "num_epochs": 10000,
+        "layers":[
+            {
+                "type": "linear",
+                "input_size": 11,
+                "output_size": 512
+            },
+            {
+                "type": "linear",
+                "input_size": 512,
+                "output_size": 1
+            }
+        ],
+        "learning_rate": {
+            "type": "simple",
+            "value": 0.01
+        },
+        "loss_fn": "root_mean_squared",
+        "dataset_reader": "csv_reader",
+        "dataset" : "monk/train.csv",
+        "batch_size": 1000
+    });
+
+    let finalOutputSize = null;
+    if(job["layers"].length){
+        var N = job["layers"].length;
+        finalOutputSize = job["layers"][N - 1]["output_size"]
+    }
+
     const jobSubmitHandler = async () => {
-        const response =  await axios.post("http://localhost:8000/start_task");
-        console.log(response)
+        const jobConfig = { "job" : job };
+        const response =  await backendClient.post("/start_task", jobConfig);
+        console.log(response.json())
+    }
+    const [seed, setSeed] = useState(1);
+    const reset = () => {
+        setSeed(Math.random());
+    }
+
+    const updateKey = (key: string, newValue: any) => {
+        setJob(prevJob => {
+            let newJob = {
+                ...prevJob,
+                key: newValue
+            };
+            return newJob
+        });
+    }
+
+    
+
+    const deleteLayer = () => {
+        console.log("Deleting layer");
+        let newLayers = job["layers"];
+        newLayers.pop();
+        reset();
+        updateKey("layers", newLayers);
+    }
+
+    const addLayer = (layer: any) => {
+        let newLayers = job["layers"];
+        newLayers.push(layer);
+        reset();
+        updateKey("layers", newLayers);
     }
 
 
     return (
-        <Button className="w-xl" onClick={jobSubmitHandler}>Submit job</Button>
+        <>
+        <Layers 
+            layers = {job["layers"]}
+            deleteLayer={deleteLayer}
+        />
+        <AddLayer 
+            key={seed}
+            addLayer={addLayer}
+            finalOutputSize={finalOutputSize}
+            />
+        <Button className="m-8 sm:m-8" onClick={jobSubmitHandler}>Submit job</Button>
+        </>
     );
 }
 
---
+++ frontend/GradFront/src/components/ui/arrow.tsx
@@ -0,0 +1,9 @@
+const Arrow = () => (
+    <div className="flex flex-col justify-center">
+        <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10.1464 2.14645C10.3417 1.95118 10.6583 1.95118 10.8536 2.14645L16.8536 7.14645C17.0488 7.34171 17.0488 7.65829 16.8536 7.85355L10.8536 13.8536C10.6583 14.0488 10.3417 14.0488 10.1464 13.8536C9.95118 13.6583 9.95118 13.3417 10.1464 13.1464L15.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H15.2929L10.1464 2.85355C9.95118 2.65829 9.95118 2.34171 10.1464 2.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"/>
+        </svg>
+    </div>
+);
+
+export default Arrow;
